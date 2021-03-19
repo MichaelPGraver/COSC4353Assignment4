463c76329510abaf9b3d051382c43ab0baef4460
Michael Graver <michaelpgraver@yahoo.com>
2021-03-18 22:51:24 -0500

    Final push assignment 3

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const bodyparser = require("body-parser");
+const multer = require("multer");
+const cookieparser = require("cookie-parser");
+const cors = require("cors");
+let form_parser = multer();
+let app = express();
+
+const {
+    SignUp,
+} = require("./endpoints");
+
+app.use(
+    cors({
+        origin: true,
+        credentials: true,
+    })
+);
+
+app.use(cookieparser());
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({ extended: true }));
+app.use(form_parser.none());
+
+app.post("/api/SignUp", SignUp);
+
+app.get('*', (req,res) =>{
+    res.sendFile(path.join(__dirname,'build','index.html'));
+});
+
+app.listen(8080);
